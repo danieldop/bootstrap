3037dab88eca486d1869ac7af1e5a0beab73a56b
Daniel Doppelt <daniel.doppelt@gmail.com>
2019-04-02 08:37:46 +0300

    kubectl v1.11.1

--- install.sh
+++ install.sh
@@ -28,7 +28,10 @@ brew install ack git git-lfs imagemagick lua lynx p7zip pigz pv rename rlwrap \
 brew cask install docker
 
 # Install k8s tools
-brew install kops kubernetes-cli kubernetes-helm stern
+brew install kops kubernetes-helm stern kubectx
+## kubectl 1.11.1
+brew install https://raw.githubusercontent.com/Homebrew/homebrew-core/750c6e49220337c1cdc26a7641853a672c4a4c01/Formula/kubernetes-cli.rb
+brew pin kubernetes-cli
 ## kubectl zsh namespace prompt
 brew tap superbrothers/zsh-kubectl-prompt
 brew install zsh-kubectl-prompt
@@ -37,6 +40,7 @@ autoload -U colors; colors
 source /usr/local/etc/zsh-kubectl-prompt/kubectl.zsh
 RPROMPT='%{$fg[blue]%}($ZSH_KUBECTL_PROMPT)%{$reset_color%}'
 END
+homebrew-core/Formula/kops.rb
 # TODO install in ~/.zshrc
 ## kubectl aliases
 git clone https://github.com/ahmetb/kubectl-aliases.git --depth=1
@@ -45,6 +49,9 @@ echo "[ -f ~/.kubectl_aliases ] && source ~/.kubectl_aliases" >> ${HOME}/.zshrc
 rm -rf ./kubectl-aliases
 ## kubectl namespace selector
 curl https://raw.githubusercontent.com/blendle/kns/master/bin/kns -o /usr/local/bin/kns && chmod +x $_
+## Helm plugins
+helm plugin install https://github.com/mstrzele/helm-edit
+helm plugin install https://github.com/databus23/helm-diff --version master
 ## Telepresence
 brew cask install osxfuse
 brew install datawire/blackbird/telepresence
