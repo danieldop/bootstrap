f8d3fd4b2381c6fd08ecc9ee3719d8bd787bde7f
Max Martyn <max.martyn@bizzabo.com>
2021-02-12 10:26:39 +0200

    Install nvm with brew

--- web.sh
+++ web.sh
@@ -6,7 +6,7 @@
 sudo -v
 
 # Keep-alive: update existing `sudo` time stamp until the script has finished.
-while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+# while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
 # Check for Homebrew,
 # Install if we don't have it
@@ -18,7 +18,27 @@ fi
 # Make sure we’re using the latest Homebrew.
 brew update
 
-brew install node@8
+# Install and setup nvm - Node Version Manager
+brew install nvm
+
+mkdir ~/.nvm
+
+# NVM setup
+export NVM_DIR="$HOME/.nvm"
+[ -s "/usr/local/opt/nvm/nvm.sh" ] && . "/usr/local/opt/nvm/nvm.sh"  # This loads nvm
+[ -s "/usr/local/opt/nvm/etc/bash_completion.d/nvm" ] && . "/usr/local/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
+
+# Install lts release of Node
+nvm install --lts
+
+# Adding all nvm related variables to .zshrc
+# If we add them in "NVM setup" step and "source .zshrc" the script will crash.
+echo "
+export NVM_DIR="$HOME/.nvm"
+[ -s "/usr/local/opt/nvm/nvm.sh" ] && . "/usr/local/opt/nvm/nvm.sh"  # This loads nvm
+[ -s "/usr/local/opt/nvm/etc/bash_completion.d/nvm" ] && . "/usr/local/opt/nvm/etc/bash_completion.d/nvm"" >> ~/.zshrc
 
 # Remove outdated versions from the cellar.
 brew cleanup
+
+exec zsh
