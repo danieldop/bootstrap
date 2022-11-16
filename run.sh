fb7d8d31353ef468a69412e31be81c5cc947871b
Daniel Doppelt <daniel.doppelt@gmail.com>
2022-11-16 06:38:13 +0200

    rename force

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 curl -LO https://github.com/danieldop/bootstrap/archive/warp_terminal.zip
 unzip warp_terminal.zip
 rm -rf warp_terminal.zip
-mv bootstrap-warp_terminal ~/bootstrap -y
+mv -f bootstrap-warp_terminal ~/bootstrap -y
 chmod +x ~/bootstrap
 cd ~/bootstrap
 ./bootstrap.sh
