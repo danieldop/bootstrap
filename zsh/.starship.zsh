a8f601a362b1d73105db8557fe9e3c13d5e8e224
Daniel Doppelt <daniel.doppelt@gmail.com>
2023-09-26 11:58:59 +0300

    update starship update k8s update brew

--- zsh/.starship.zsh
+++ zsh/.starship.zsh
@@ -2,24 +2,37 @@
 LFILE="/etc/*-release"
 MFILE="/System/Library/CoreServices/SystemVersion.plist"
 if [[ -f $LFILE ]]; then
-_distro=$(awk '/^ID=/' /etc/*-release | awk -F'=' '{ print tolower($2) }')
+  _distro=$(awk '/^ID=/' /etc/*-release | awk -F'=' '{ print tolower($2) }')
 elif [[ -f $MFILE ]]; then
-_distro="macos"
+  _distro="macos"
 fi
 
 # set an icon based on the distro
 # make sure your font is compatible with https://github.com/lukas-w/font-logos
 case $_distro in
-*debian*)                ICON="";;
-*raspbian*)              ICON="";;
-*ubuntu*)                ICON="";;
-*fedora*)                ICON="";;
-*coreos*)                ICON="";;
-*centos*)                ICON="";;
-*alpine*)                ICON="";;
-*rhel*)                  ICON="";;
-*macos*)                 ICON="";;
-*)                       ICON="";;
+    *kali*)                  ICON="ﴣ";;
+    *arch*)                  ICON="";;
+    *debian*)                ICON="";;
+    *raspbian*)              ICON="";;
+    *ubuntu*)                ICON="";;
+    *elementary*)            ICON="";;
+    *fedora*)                ICON="";;
+    *coreos*)                ICON="";;
+    *gentoo*)                ICON="";;
+    *mageia*)                ICON="";;
+    *centos*)                ICON="";;
+    *opensuse*|*tumbleweed*) ICON="";;
+    *sabayon*)               ICON="";;
+    *slackware*)             ICON="";;
+    *linuxmint*)             ICON="";;
+    *alpine*)                ICON="";;
+    *aosc*)                  ICON="";;
+    *nixos*)                 ICON="";;
+    *devuan*)                ICON="";;
+    *manjaro*)               ICON="";;
+    *rhel*)                  ICON="";;
+    *macos*)                 ICON="";;
+    *)                       ICON="";;
 esac
 
 export STARSHIP_DISTRO="$ICON"
